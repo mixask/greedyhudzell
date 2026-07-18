3c91861a9aa211d73c68389e0c212a03dda7b324
mixask <mixa21skob@gmail.com>
2026-07-18 23:37:57 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
+// Отдаём только обфусцированный скрипт
+app.get('/script.lua', (req, res) => {
+  try {
+    const script = fs.readFileSync(SCRIPT_OBFUSCATED_PATH, 'utf8');
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.send(script);
+  } catch {
+    buildScript();
+    const script = fs.readFileSync(SCRIPT_OBFUSCATED_PATH, 'utf8');
+    res.send(script);
+  }
+});
 
+// Блокируем доступ к исходному коду
+app.get('/script-source.lua', (req, res) => {
+  res.status(403).send('⛔ Access Denied. Source code is not public.');
+});
